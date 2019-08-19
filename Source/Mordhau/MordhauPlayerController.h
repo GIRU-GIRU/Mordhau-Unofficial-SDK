d4204a93554ce3a726c7d68f8641d6e2d92f8da7
BattlefieldRecapitator <54209002+BattlefieldRecapitator@users.noreply.github.com>
2019-08-19 12:56:41 -0700

    New Placeholders
    Bot Profiles, Bot Behavior Profiles, Mordhau Equipment, Mordhau EquipmentPart, Mordhau Inventory Item, Mordhau Wearable, the rest are not filled out but necessary for compiling

---
+++ Source/Mordhau/MordhauPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MordhauPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MORDHAU_API AMordhauPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
